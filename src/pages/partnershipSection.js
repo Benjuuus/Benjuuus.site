f232fc3636ba50b78e7939e9e4d07ef2e2c0995e
Benjamin7780 <benjaminm7780@gmail.com>
2023-04-28 01:19:05 -0300

    ''

--- src/pages/partnershipSection.js
+++ src/pages/partnershipSection.js
@@ -1,13 +1,20 @@
-import {sites} from '../pages/api/sites';
+import { sites } from '../pages/api/sites';
+import styles from '../styles/partnershipSection.module.css'
 
 export default function Partnership(){
     return(
         <>
-            {sites?.map(el=>{
-                return(<>
-
-                </>)
-            })}
+            <div id={styles.partnershipContainer}>
+                {sites?.map((el, index)=>{
+                    return(<>
+                        <div key={index} className={styles.PartnerContainer}>
+                            <a href={el.link}></a>
+                            <img src={el.image} alt={el.name}></img>
+                            <p className={styles.PartnerCode}>{el.partnerCode}</p>
+                        </div>
+                    </>)
+                })}
+            </div>
         </>
     )
 }
